941f7d0ed7e03840fcbb5782a75c2a74ff0dc9b7
Shivam Shukla <shivamshukla3108@gmail.com>
2022-03-27 10:20:46 +0530

    Lecture 26

---
+++ Lecture 26/myProject/myProject/views.py
@@ -0,0 +1,67 @@
+
+from django.http import HttpResponse  
+from django.shortcuts import render
+
+
+
+def index(request):
+    return render(request,'index.html');
+
+def aboutus(request):
+    return render(request,'about-us.html');
+
+def services(request):
+    return render(request,'services.html');
+
+
+# ...........................................
+
+
+def gallery(request):
+    return render(request,'gallery.html');
+
+def contact(request):
+    return render(request,'contact.html');
+
+def userform(request):
+    data={}  # its must because if not passes values to data variable is like error, because its not showing anywhere and directly we use it.
+    try:
+        if request.method=="POST":
+            # Both ways working correctly!
+            val1=int(request.POST["firstvalue"]);
+            val2=int(request.POST["secondvalue"]);
+            # val1=int(request.POST.get("firstvalue"));
+            # val2=int(request.POST.get("secondvalue"));
+            print(val1+val2); 
+            data={
+                'firstvalue' : val1,
+                'secondvalue' : val2,
+                'addition' : val1+val2,
+                'multiplication' : val1*val2,
+            }
+    except:
+        pass
+    return render(request,'USERFORM.html',data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
